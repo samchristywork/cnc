b7c42d88954501b09c345d37bc3151df94311732
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-09-17 21:37:40 -0500

    Fixed bug in script.
    I forgot to specify the "G1" for these commands. This has caused
    confusion for me when using the script as a reference.

--- scripts/python_example.py
+++ scripts/python_example.py
@@ -12,9 +12,9 @@ send_gcode(b"G21") # Millimeters
 #send_gcode(b"G91") # Relative motion
 send_gcode(b"G90") # Absolute motion
 send_gcode(b"G92 X0 Y0 Z0") # Specify that the head is at (0,0,0)
-send_gcode(b"F20 X0 Y0 Z-5")
+send_gcode(b"G1 F20 X0 Y0 Z-5")
 #send_gcode(b"M3 S0") # Set spindle speed. This is required for other speed operations
-#send_gcode(b"F20 X5 Y0 Z0 S10")
+#send_gcode(b"G1 F20 X5 Y0 Z0 S10")
 #send_gcode(b"M3 S0") # Set spindle speed to 0
 #send_gcode(b"G0 X0 Y0 Z0") # Go home
 s.close()
