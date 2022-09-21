11a7d71e74abf715dbb755d79f872d16005d22a9
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-09-20 22:22:02 -0500

    Hours of work. Fuck.

--- src/main.cpp
+++ src/main.cpp
@@ -96,12 +96,20 @@ void realize(GtkWidget *widget) {
   gtk_gl_area_make_current(GTK_GL_AREA(widget));
 
   glGenVertexArrays(1, &buffer);
-  glBindVertexArray(buffer);
-
   glGenBuffers(1, &buffer);
+
+  glBindVertexArray(buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
 
   init_shaders(&program);
 
@@ -129,14 +137,9 @@ void draw(GLuint program) {
   glm::mat4 projection = glm::perspective(45.0, aspect_ratio, 0.1, 100.0);
   glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, &projection[0][0]);
 
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+  glBindVertexArray(buffer);
   glDrawArrays(GL_TRIANGLES, 0, 36);
 
-  glDisableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glUseProgram(0);
 }
