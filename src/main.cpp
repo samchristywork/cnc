47fbc88f60fc5a394a2edfd0fecbef857781951a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-09-22 19:40:26 -0500

    Switched from forking to using pthreads.

--- src/main.cpp
+++ src/main.cpp
@@ -179,156 +179,154 @@ void add_button(GtkWidget *box, const char *label, int movement_type, const char
   gtk_container_add(GTK_CONTAINER(box), button);
 }
 
+void *render_3d(void *v) {
+  /*
+   * Thread that handles rendering the drill bit.
+   */
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetTraceLogLevel(LOG_NONE);
+  InitWindow(0, 0, "CNC");
+
+  Camera camera = {0};
+  camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+  Model model1 = LoadModel("models/collet.obj");
+  Model model2 = LoadModel("models/bit.obj");
+  Model model3 = LoadModel("models/chuck.obj");
+
+  Shader shader = LoadShader(0, "shaders/grayscale.fs");
+  model3.materials[0].shader = shader;
+
+  //Texture2D texture = LoadTexture("untitled.png");
+  //model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+  SetCameraMode(camera, CAMERA_FREE);
+
+  SetTargetFPS(60);
+
+  Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
+  Vector3 cubeSize = {1.0f, 1.0f, 1.0f};
+  Vector2 cubeScreenPosition = {0.0f, 0.0f};
+
+  Ray ray = {0};
+
+  RayCollision collision = {0};
+
+  float angle = 0;
+
+  while (!WindowShouldClose()) {
+    UpdateCamera(&camera);
+    if (IsKeyDown('Q')) {
+      camera.target.y -= .1;
+    }
+    if (IsKeyDown('E')) {
+      camera.target.y += .1;
+    }
+    if (IsKeyDown('D')) {
+      angle += .1;
+    }
+    camera.position.x = 10.f * sin(angle);
+    camera.position.z = 10.f * cos(angle);
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      if (!collision.hit) {
+        ray = GetMouseRay(GetMousePosition(), camera);
+
+        collision = GetRayCollisionBox(ray,
+                                       (BoundingBox){(Vector3){cubePosition.x - cubeSize.x / 2, cubePosition.y - cubeSize.y / 2, cubePosition.z - cubeSize.z / 2},
+                                                     (Vector3){cubePosition.x + cubeSize.x / 2, cubePosition.y + cubeSize.y / 2, cubePosition.z + cubeSize.z / 2}});
+      } else
+        collision.hit = false;
+    }
+    cubeScreenPosition = GetWorldToScreen((Vector3){cubePosition.x, cubePosition.y + 2.5f, cubePosition.z}, camera);
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawFPS(10, 10);
+    BeginMode3D(camera);
+
+    DrawModelEx(model1, {0, 0, 0}, {1, 0, 0}, 180, {4, 4, 4}, CLITERAL(Color){200, 100, 100, 255});
+    DrawModelWiresEx(model1, {0, 0, 0}, {1, 0, 0}, 180, {4, 4, 4}, CLITERAL(Color){130, 30, 30, 255});
+    DrawModelEx(model2, {0, 0, 0}, {1, 0, 0}, 90, {1, 1, 1}, CLITERAL(Color){100, 200, 100, 255});
+    DrawModelWiresEx(model2, {0, 0, 0}, {1, 0, 0}, 90, {1, 1, 1}, CLITERAL(Color){30, 130, 30, 255});
+    DrawModelEx(model3, {0, 0, 0}, {0, 0, 1}, -90, {.75, .75, .75}, CLITERAL(Color){100, 100, 200, 255});
+    DrawModelWiresEx(model3, {0, 0, 0}, {0, 0, 1}, -90, {.75, .75, .75}, CLITERAL(Color){30, 30, 130, 255});
+
+    DrawGrid(100, 1.0f);
+
+    EndMode3D();
+    DrawText("Drill Speed: 100 / 100", (int)cubeScreenPosition.x - MeasureText("Enemy: 100/100", 20) / 2, (int)cubeScreenPosition.y, 20, BLACK);
+    if (collision.hit)
+      DrawText("BOX SELECTED", 10, 10, 30, GREEN);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
 
-  pid_t pid = fork();
-  if (pid < 0) {
-    die();
-  }
-  if (pid) {
+  pthread_t thread;
+  pthread_create(&thread, NULL, render_3d, NULL);
 
-    /*
-     * Thread that handles rendering the drill bit.
-     */
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    SetTraceLogLevel(LOG_NONE);
-    InitWindow(0, 0, "CNC");
+  /*
+   * Thread that handles rendering the GTK gui.
+   */
+  usleep(1000 * 1000);
 
-    Camera camera = {0};
-    camera.position = (Vector3){10.0f, 10.0f, 10.0f};
-    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-    Model model1 = LoadModel("models/collet.obj");
-    Model model2 = LoadModel("models/bit.obj");
-    Model model3 = LoadModel("models/chuck.obj");
+  ports_init();
 
-    Shader shader = LoadShader(0, "shaders/grayscale.fs");
-    model3.materials[0].shader = shader;
+  gtk_init(&argc, &argv);
 
-    //Texture2D texture = LoadTexture("untitled.png");
-    //model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-    SetCameraMode(camera, CAMERA_FREE);
+  // Main window
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_resizable(GTK_WINDOW(window), false);
+  gtk_window_set_title(GTK_WINDOW(window), "CNC");
 
-    SetTargetFPS(60);
+  // Boxes
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, FALSE);
+  gtk_container_add(GTK_CONTAINER(window), box);
+  GtkWidget *hbox1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, FALSE);
+  GtkWidget *hbox2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, FALSE);
 
-    Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
-    Vector3 cubeSize = {1.0f, 1.0f, 1.0f};
-    Vector2 cubeScreenPosition = {0.0f, 0.0f};
+  // Buttons
+  add_button(box, "Start Drill", START_DRILL, "start_drill_button");
+  add_button(box, "Stop Drill", STOP_DRILL, "stop_drill_button");
+  gtk_container_add(GTK_CONTAINER(box), hbox1);
+  add_button(hbox1, "Z- (Q)", ZM, "zm_button");
+  add_button(hbox1, "Y+ (W)", YP, "yp_button");
+  add_button(hbox1, "Z+ (E)", ZP, "zp_button");
+  gtk_container_add(GTK_CONTAINER(box), hbox2);
+  add_button(hbox2, "X- (A)", XM, "xm_button");
+  add_button(hbox2, "Y- (S)", YM, "ym_button");
+  add_button(hbox2, "X+ (D)", XP, "xp_button");
 
-    Ray ray = {0};
+  // Sliders
+  GtkWidget *drill_speed_slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 10000, .01);
+  g_signal_connect(drill_speed_slider, "value-changed", G_CALLBACK(drill_speed_slider_callback), NULL);
+  gtk_widget_set_name(drill_speed_slider, "drill_speed_slider");
+  gtk_container_add(GTK_CONTAINER(box), drill_speed_slider);
 
-    RayCollision collision = {0};
+  GtkWidget *movement_speed_slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 3, .001);
+  g_signal_connect(movement_speed_slider, "value-changed", G_CALLBACK(movement_speed_slider_callback), NULL);
+  gtk_widget_set_name(movement_speed_slider, "movement_speed_slider");
+  gtk_container_add(GTK_CONTAINER(box), movement_speed_slider);
 
-    float angle = 0;
+  // Abort
+  add_button(box, "ABORT", ABORT, "abort_button");
 
-    while (!WindowShouldClose()) {
-      UpdateCamera(&camera);
-      if (IsKeyDown('Q')) {
-        camera.target.y -= .1;
-      }
-      if (IsKeyDown('E')) {
-        camera.target.y += .1;
-      }
-      if (IsKeyDown('D')) {
-        angle += .1;
-      }
-      camera.position.x = 10.f * sin(angle);
-      camera.position.z = 10.f * cos(angle);
-      if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        if (!collision.hit) {
-          ray = GetMouseRay(GetMousePosition(), camera);
+  // CSS
+  GtkCssProvider *css = gtk_css_provider_new();
+  gtk_css_provider_load_from_path(css, "style.css", NULL);
+  gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-          collision = GetRayCollisionBox(ray,
-                                         (BoundingBox){(Vector3){cubePosition.x - cubeSize.x / 2, cubePosition.y - cubeSize.y / 2, cubePosition.z - cubeSize.z / 2},
-                                                       (Vector3){cubePosition.x + cubeSize.x / 2, cubePosition.y + cubeSize.y / 2, cubePosition.z + cubeSize.z / 2}});
-        } else
-          collision.hit = false;
-      }
-      cubeScreenPosition = GetWorldToScreen((Vector3){cubePosition.x, cubePosition.y + 2.5f, cubePosition.z}, camera);
-      BeginDrawing();
-      ClearBackground(RAYWHITE);
-      DrawFPS(10, 10);
-      BeginMode3D(camera);
+  // Events
+  gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
+  g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(keypress_callback), NULL);
 
-      DrawModelEx(model1, {0, 0, 0}, {1, 0, 0}, 180, {4, 4, 4}, CLITERAL(Color){200, 100, 100, 255});
-      DrawModelWiresEx(model1, {0, 0, 0}, {1, 0, 0}, 180, {4, 4, 4}, CLITERAL(Color){130, 30, 30, 255});
-      DrawModelEx(model2, {0, 0, 0}, {1, 0, 0}, 90, {1, 1, 1}, CLITERAL(Color){100, 200, 100, 255});
-      DrawModelWiresEx(model2, {0, 0, 0}, {1, 0, 0}, 90, {1, 1, 1}, CLITERAL(Color){30, 130, 30, 255});
-      DrawModelEx(model3, {0, 0, 0}, {0, 0, 1}, -90, {.75, .75, .75}, CLITERAL(Color){100, 100, 200, 255});
-      DrawModelWiresEx(model3, {0, 0, 0}, {0, 0, 1}, -90, {.75, .75, .75}, CLITERAL(Color){30, 30, 130, 255});
-
-      DrawGrid(100, 1.0f);
-
-      EndMode3D();
-      DrawText("Drill Speed: 100 / 100", (int)cubeScreenPosition.x - MeasureText("Enemy: 100/100", 20) / 2, (int)cubeScreenPosition.y, 20, BLACK);
-      if (collision.hit)
-        DrawText("BOX SELECTED", 10, 10, 30, GREEN);
-      EndDrawing();
-    }
-
-    CloseWindow();
-  } else {
-
-    /*
-     * Thread that handles rendering the GTK gui.
-     */
-    usleep(1000 * 1000);
-
-    ports_init();
-
-    gtk_init(&argc, &argv);
-
-    // Main window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_resizable(GTK_WINDOW(window), false);
-    gtk_window_set_title(GTK_WINDOW(window), "CNC");
-
-    // Boxes
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, FALSE);
-    gtk_container_add(GTK_CONTAINER(window), box);
-    GtkWidget *hbox1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, FALSE);
-    GtkWidget *hbox2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, FALSE);
-
-    // Buttons
-    add_button(box, "Start Drill", START_DRILL, "start_drill_button");
-    add_button(box, "Stop Drill", STOP_DRILL, "stop_drill_button");
-    gtk_container_add(GTK_CONTAINER(box), hbox1);
-    add_button(hbox1, "Z- (Q)", ZM, "zm_button");
-    add_button(hbox1, "Y+ (W)", YP, "yp_button");
-    add_button(hbox1, "Z+ (E)", ZP, "zp_button");
-    gtk_container_add(GTK_CONTAINER(box), hbox2);
-    add_button(hbox2, "X- (A)", XM, "xm_button");
-    add_button(hbox2, "Y- (S)", YM, "ym_button");
-    add_button(hbox2, "X+ (D)", XP, "xp_button");
-
-    // Sliders
-    GtkWidget *drill_speed_slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 10000, .01);
-    g_signal_connect(drill_speed_slider, "value-changed", G_CALLBACK(drill_speed_slider_callback), NULL);
-    gtk_widget_set_name(drill_speed_slider, "drill_speed_slider");
-    gtk_container_add(GTK_CONTAINER(box), drill_speed_slider);
-
-    GtkWidget *movement_speed_slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 3, .001);
-    g_signal_connect(movement_speed_slider, "value-changed", G_CALLBACK(movement_speed_slider_callback), NULL);
-    gtk_widget_set_name(movement_speed_slider, "movement_speed_slider");
-    gtk_container_add(GTK_CONTAINER(box), movement_speed_slider);
-
-    // Abort
-    add_button(box, "ABORT", ABORT, "abort_button");
-
-    // CSS
-    GtkCssProvider *css = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(css, "style.css", NULL);
-    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
-
-    // Events
-    gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
-    g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(keypress_callback), NULL);
-
-    // Main loop
-    gtk_widget_show_all(GTK_WIDGET(window));
-    gtk_main();
-  }
+  // Main loop
+  gtk_widget_show_all(GTK_WIDGET(window));
+  gtk_main();
 }
