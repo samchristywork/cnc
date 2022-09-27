7e6d8b8cafe51492a079aa989b81a0e19a60acd3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-09-26 20:42:07 -0500

    Replaced older example with this new one.
    The old example didn't capture how to read back data from GRBL.

--- scripts/python_example.py
+++ scripts/python_example.py
@@ -1,20 +1,27 @@
 import serial
 import time
 
-s=serial.Serial("/dev/ttyUSB0", 115200)
+s = serial.Serial('/dev/ttyUSB0', 115200)
 
-def send_gcode(g):
-    s.write(g+b"\r\n")
+s.write(b"\r\n\r\n")
+time.sleep(2)
+s.flushInput()
+
+s.write(b'M3 S100\n')
+out = s.readline()
+print(out)
+
+time.sleep(1)
+
+s.write(b'M3 S0\n')
+out = s.readline()
+print(out)
+
+time.sleep(1)
+
+s.write(b'$\n')
+out = s.readline()
+print(out)
 
 time.sleep(2)
-#send_gcode(b"G28 X0 Y0 Z0") # Home all axes?
-send_gcode(b"G21") # Millimeters
-#send_gcode(b"G91") # Relative motion
-send_gcode(b"G90") # Absolute motion
-send_gcode(b"G92 X0 Y0 Z0") # Specify that the head is at (0,0,0)
-send_gcode(b"G1 F20 X0 Y0 Z-5")
-#send_gcode(b"M3 S0") # Set spindle speed. This is required for other speed operations
-#send_gcode(b"G1 F20 X5 Y0 Z0 S10")
-#send_gcode(b"M3 S0") # Set spindle speed to 0
-#send_gcode(b"G0 X0 Y0 Z0") # Go home
 s.close()
